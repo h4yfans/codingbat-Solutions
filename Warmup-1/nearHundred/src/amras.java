faac754da174bd6c2497c79279b8ec9b5ca56b7f
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-17 18:00:54 +0300

    nearHundred

--- Warmup-1/nearHundred/src/amras.java
+++ Warmup-1/nearHundred/src/amras.java
@@ -1,11 +1,25 @@
 /**
  * Created by KAAN on 17/05/2016.
- *
+ * <p>
  * Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
-
- nearHundred(93) → true
- nearHundred(90) → true
- nearHundred(89) → false
+ * <p>
+ * nearHundred(93) → true
+ * nearHundred(90) → true
+ * nearHundred(89) → false
  */
+
 public class amras {
+    public static void main(String[] args) {
+        System.out.println(nearHundred(93));
+        System.out.println(nearHundred(90));
+        System.out.println(nearHundred(89));
+    }
+
+    public static boolean nearHundred(int n) {
+
+        return Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10;
+
+    }
+
+
 }
