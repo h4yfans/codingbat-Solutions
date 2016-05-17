4ef2018d70fa60af1c087c138a6e23e34fcde88d
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-17 21:30:53 +0300

    icyHot

---
+++ Warmup-1/icyHot/src/amras.java
@@ -0,0 +1,20 @@
+/**
+ * Created by KAAN on 17/05/2016.
+ * <p>
+ * Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+ * <p>
+ * icyHot(120, -1) → true
+ * icyHot(-1, 120) → true
+ * icyHot(2, 120) → false
+ */
+public class amras {
+    public static void main(String[] args) {
+
+
+    }
+
+    public boolean icyHot(int temp1, int temp2) {
+        return (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 100);
+    }
+
+}
