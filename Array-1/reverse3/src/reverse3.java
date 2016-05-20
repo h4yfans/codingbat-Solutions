6eea3d1b13a5f999c437f37ca357e7e0e9dc797c
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-20 18:41:56 +0300

    reverse3

---
+++ Array-1/reverse3/src/reverse3.java
@@ -0,0 +1,16 @@
+/**
+ * Created by KAAN on 20/05/2016.
+ * Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+ * reverse3([1, 2, 3]) → [3, 2, 1]
+ * reverse3([5, 11, 9]) → [9, 11, 5]
+ * reverse3([7, 0, 0]) → [0, 0, 7]
+ */
+public class reverse3 {
+    public int[] reverse3(int[] nums) {
+
+        int[] newArr = {nums[2], nums[1], nums[0]};
+        return newArr;
+    }
+
+
+}
