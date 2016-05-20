834d136b4251ea1be930cc5371ff0eb631e4d5bb
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-20 17:18:22 +0300

    makePi

---
+++ Array-1/makePi/src/makePi.java
@@ -0,0 +1,15 @@
+/**
+ * Created by KAAN on 20/05/2016.
+ *
+ * Return an int array length 3 containing the first 3 digits of pi,makePi() → [3, 1, 4]
+ * {3, 1, 4}.
+ */
+public class makePi {
+    public int[] makePi() {
+
+        int[] a = {3,1,4};
+        return a;
+
+    }
+
+}
