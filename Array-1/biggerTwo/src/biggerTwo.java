e237280b97b88de208f520e68c94cffc318d7aaf
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-20 22:07:42 +0300

    biggerTwo

---
+++ Array-1/biggerTwo/src/biggerTwo.java
@@ -0,0 +1,23 @@
+
+
+/**
+ * Created by KAAN on 20/05/2016.
+ *
+ * Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array. Return the array which has the largest sum. In event of a tie, return a.
+ * biggerTwo([1, 2], [3, 4]) → [3, 4]
+ * biggerTwo([3, 4], [1, 2]) → [3, 4]
+ * biggerTwo([1, 1], [1, 2]) → [1, 2]
+ */
+public class biggerTwo {
+    public int[] biggerTwo(int[] a, int[] b) {
+        
+        int sum = a[0] + a[1] - b[0] - b[1];
+
+        if (sum>=0)
+            return a;
+        else
+            return b;
+
+    }
+
+}
