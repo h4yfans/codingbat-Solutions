cb51485b4554b385649411ed27cc345a5084e1ab
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-21 19:27:03 +0300

    teaParty

---
+++ Logic-1/teaParty/src/teaParty.java
@@ -0,0 +1,20 @@
+/**
+ * Created by KAAN on 21/05/2016.
+ * We are having a party with amounts of tea and candy.
+ * Return the int outcome of the party encoded as 0=bad, 1=good, or 2=great.
+ * A party is good (1) if both tea and candy are at least 5.
+ * However, if either tea or candy is at least double the amount of the other one, the party is great (2).
+ * However, in all cases, if either tea or candy is less than 5, the party is always bad (0).
+ * teaParty(6, 8) → 1
+ * teaParty(3, 8) → 0
+ * teaParty(20, 6) → 2
+ */
+public class teaParty {
+    public int teaParty(int tea, int candy) {
+        if(tea < 5 || candy < 5) return 0;
+        if(tea >= candy *2 || candy >= tea *2) return 2;
+        return 1;
+
+    }
+
+}
