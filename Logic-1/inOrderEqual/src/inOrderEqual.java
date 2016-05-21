bd14947049377f3fee555ee886be7dd777546800
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-21 21:06:29 +0300

    inOrderEqual

---
+++ Logic-1/inOrderEqual/src/inOrderEqual.java
@@ -0,0 +1,18 @@
+/**
+ * Created by KAAN on 21/05/2016.
+ * <p>
+ * Given three ints, a b c, return true if they are in strict increasing order,
+ * such as 2 5 11, or 5 6 7, but not 6 5 7 or 5 5 7. However, with the exception that if "equalOk" is true,
+ * equality is allowed, such as 5 5 7 or 5 5 5.
+ * inOrderEqual(2, 5, 11, false) → true
+ * inOrderEqual(5, 7, 6, false) → false
+ * inOrderEqual(5, 5, 7, true) → true
+ */
+public class inOrderEqual {
+    public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
+
+        return (b > a && c > b) || (equalOk && (b>=a || c>=b));
+
+    }
+
+}
