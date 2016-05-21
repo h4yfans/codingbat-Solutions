8e6d2a445d2053031a0a3e37dcb0ef3b00304275
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-21 20:33:11 +0300

    inOrder

---
+++ Logic-1/inOrder/src/inOrder.java
@@ -0,0 +1,17 @@
+/**
+ * Created by KAAN on 21/05/2016.
+ * Given three ints, a b c, return true if b is greater than a, and c is greater than b.
+ * However, with the exception that if "bOk" is true, b does not need to be greater than a.
+ * inOrder(1, 2, 4, false) → true
+ * inOrder(1, 2, 1, false) → false
+ * inOrder(1, 1, 2, true) → true
+ * Go
+ */
+public class inOrder {
+    public boolean inOrder(int a, int b, int c, boolean bOk) {
+
+        return (b>a && c > b) || (bOk && c > b);
+
+    }
+
+}
