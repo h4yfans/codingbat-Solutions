8cd822fd2060471d9ac1fbbe5e0aac68ff6e3283
Kaan Karaca <kaan94karaca@gmail.com>
2016-06-02 03:26:16 +0300

    hard

---
+++ Array-3/maxMirror/src/maxMirror.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Kaan Karaca on 01/06/2016.
+ * github.com/h4yfans
+ * kaan94karaca@gmail.com
+ *
+ * We'll say that a "mirror" section in an array is a group of contiguous elements such that somewhere in the array,
+ * the same group appears in reverse order. For example, the largest mirror section in {1, 2, 3, 8, 9, 3, 2, 1} is length 3 (the {1, 2, 3} part).
+ * Return the size of the largest mirror section found in the given array.
+ * maxMirror([1, 2, 3, 8, 9, 3, 2, 1]) → 3
+ * maxMirror([1, 2, 1, 4]) → 3
+ * maxMirror([7, 1, 2, 9, 7, 2, 1]) → 2
+ */
+public class maxMirror {
+    public int maxMirror(int[] nums) {
+
+
+
+
+    }
+
+}
