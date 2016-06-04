d2c788ba16cb16ed62cff5b10b00ecc905996622
Kaan Karaca <kaan94karaca@gmail.com>
2016-06-04 15:06:57 +0300

    fibonacci

---
+++ Recursion-1/fibonacci/src/fibonacci.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Kaan Karaca on 04/06/2016.
+ * github.com/h4yfans
+ * kaan94karaca@gmail.com
+ *
+ * The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition.
+ * The first two values in the sequence are 0 and 1 (essentially 2 base cases).
+ * Each subsequent value is the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on.
+ * Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence.
+ * fibonacci(0) → 0
+ * fibonacci(1) → 1
+ * fibonacci(2) → 1
+ */
+public class fibonacci {
+    public int fibonacci(int n) {
+
+        if (n<=1) return n;
+
+        return fibonacci(n-1) + fibonacci(n-2);
+
+    }
+
+}
