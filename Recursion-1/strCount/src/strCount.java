2158090cad336b8822409f5fa0f598fff74cff8a
Kaan Karaca <kaan94karaca@gmail.com>
2016-06-05 00:07:24 +0300

    strCount

---
+++ Recursion-1/strCount/src/strCount.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Kaan Karaca on 04/06/2016.
+ * github.com/h4yfans
+ * kaan94karaca@gmail.com
+ * <p>
+ * Given a string and a non-empty substring sub, compute recursively the number of times that sub appears in
+ * the string, without the sub strings overlapping.
+ * strCount("catcowcat", "cat") → 2
+ * strCount("catcowcat", "cow") → 1
+ * strCount("catcowcat", "dog") → 0
+ */
+public class strCount {
+    public int strCount(String str, String sub) {
+        
+        if (str.length() < sub.length())
+            return 0;
+
+        if (str.substring(0, sub.length()).equals(sub))
+            return 1 + strCount(str.substring(sub.length()), sub);
+
+
+        return strCount(str.substring(1), sub);
+    }
+
+
+}
