3391d5cad25b93e644f077e46666a61f31ab7e03
Kaan Karaca <kaan94karaca@gmail.com>
2016-06-04 20:20:17 +0300

    array11

---
+++ Recursion-1/array11/src/array11.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Kaan Karaca on 04/06/2016.
+ * github.com/h4yfans
+ * kaan94karaca@gmail.com
+ * <p>
+ * Given an array of ints, compute recursively the number of times that the value 11 appears in the array.
+ * We'll use the convention of considering only the part of the array that begins at the given index. In this way,
+ * a recursive call can pass index+1 to move down the array. The initial call will pass in index as 0.
+ * array11([1, 2, 11], 0) → 1
+ * array11([11, 11], 0) → 2
+ * array11([1, 2, 3, 4], 0) → 0
+ */
+public class array11 {
+    public int array11(int[] nums, int index) {
+
+        if (index >= nums.length) return 0;
+
+        if (nums[index] == 11)
+            return 1 + array11(nums,index+1);
+
+
+        return array11(nums,index+1);
+
+
+    }
+
+}
