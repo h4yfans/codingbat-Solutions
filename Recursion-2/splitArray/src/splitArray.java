723e48739f75eb7b5fe9d4423184ed79a1b46ad1
Kaan Karaca <kaan94karaca@gmail.com>
2016-06-05 21:10:10 +0300

    splitArray

---
+++ Recursion-2/splitArray/src/splitArray.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Kaan Karaca on 05/06/2016.
+ * github.com/h4yfans
+ * kaan94karaca@gmail.com
+ *
+ * Given an array of ints, is it possible to divide the ints into two groups, so that the sums of the two groups are the same.
+ * Every int must be in one group or the other. Write a recursive helper method that takes whatever arguments you like,
+ * and make the initial call to your recursive helper from splitArray(). (No loops needed.)
+ splitArray([2, 2]) → true
+ splitArray([2, 3]) → false
+ splitArray([5, 2, 3]) → true
+ */
+public class splitArray {
+    public boolean splitArray(int[] nums) {
+        return splitArrayHelper(0, nums, 0, 0);
+    }
+
+    public boolean splitArrayHelper(int start, int[] nums, int group1,
+                                    int group2) {
+        if(start >= nums.length)
+            return group1 == group2;
+
+        if(splitArrayHelper(start+1, nums, group1 + nums[start], group2))
+            return true;
+
+        if(splitArrayHelper(start+1, nums, group1, group2 + nums[start]))
+            return true;
+
+        return false;
+    }
+
+
+
+}
