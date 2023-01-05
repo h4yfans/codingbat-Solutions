eac9a54abd292231a43b9f2f37a7debca096e9f9
ayzeqq <99403758+ayzeqq@users.noreply.github.com>
2023-01-06 00:16:55 +0300

    Update countTriple.java

--- String-3/countTriple/src/countTriple.java
+++ String-3/countTriple/src/countTriple.java
@@ -12,7 +12,6 @@ public class countTriple {
     public int countTriple(String str) {
 
         int count = 0;
-        if (str.length() < 3) return 0;
 
         for (int i = 0; i < str.length()-2; i++) {
             if (str.charAt(i)==str.charAt(i+1) && str.charAt(i)==str.charAt(i+2))
